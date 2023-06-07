00bd816ffe8947ca0b50fe32ef3bb96ba6d99884
Goktug Poyraz <goktug.poyraz@consensys.net>
2023-06-07 13:41:34 +0200

    fix tsc lint issues

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -3495,7 +3495,7 @@ export function rejectPendingApproval(
  * @param messageList - The list of messages to reject
  */
 export function rejectAllMessages(
-  messageList: TemporaryMessageDataType[],
+  messageList: [],
 ): ThunkAction<void, MetaMaskReduxState, unknown, AnyAction> {
   return async (dispatch: MetaMaskReduxDispatch) => {
     return Promise.all(
